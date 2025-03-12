6ef7becc11db7a097c10621f6583666bdb73b7c9
upio <75510171+notpoiu@users.noreply.github.com>
2025-03-12 00:22:22 -0400

    fix: actually fixed organization

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,27 +15,20 @@ const inter = Inter({
 const docsTree: PageTree.Root = {
   name: 'Documentation',
   children: [
+    { type: 'separator', name: 'Guides' },
+    { type: 'page', name: 'Home', url: '/' },
+    { type: 'page', name: 'Key System', url: '/key-system' },
+
+    { type: 'separator', name: 'Addons' },
+    { type: 'page', name: 'Getting Started', url: '/addons/getting_started' },
+    { type: 'page', name: 'API', url: '/addons/api' },
+
+    { type: 'separator', name: 'Purchase' },
+    { type: 'page', name: 'Getting Started', url: '/purchase/getting_started' },
     {
       type: 'folder',
-      name: 'Guides',
+      name: 'Payment Methods',
       children: [
-        { type: 'page', name: 'Home', url: '/home' },
-        { type: 'page', name: 'Key System', url: '/key-system' }
-      ]
-    },
-    {
-      type: 'folder',
-      name: 'Addons',
-      children: [
-        { type: 'page', name: 'Getting Started', url: '/addons/getting_started' },
-        { type: 'page', name: 'API', url: '/addons/api' }
-      ]
-    },
-    {
-      type: 'folder',
-      name: 'Purchase',
-      children: [
-        { type: 'page', name: 'Getting Started', url: '/purchase/getting_started' },
         { type: 'page', name: 'Official Shop', url: '/purchase/sellapp' },
         { type: 'page', name: 'Bloxproducts', url: '/purchase/bloxproducts' },
         { type: 'page', name: 'Robux', url: '/purchase/robux' }
