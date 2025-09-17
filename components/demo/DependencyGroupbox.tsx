eea04f33a992a82b5eabb524bb70dfa685d34618
upio <75510171+notpoiu@users.noreply.github.com>
2025-09-16 23:05:52 -0400

    perf: improve performance by moving code into memoized components & not using UIStateProvider when not needed

--- components/demo/DependencyGroupbox.tsx
+++ components/demo/DependencyGroupbox.tsx
@@ -41,6 +41,7 @@ export default function DemoDependencyBox() {
     ),
     []
   );
+
   return (
     <div className="max-w-[720px] max-h-[250px]">
       <div className="flex flex-col gap-4 border rounded-lg p-4 h-[250px] my-auto items-center">
