8e393b7f49d58365d37bb4c63cec506c6ea50e4d
notpoiu <75510171+notpoiu@users.noreply.github.com>
2026-02-19 22:53:53 -0500

    feat: update docs for divider

--- components/obsidian/elements/Divider.tsx
+++ components/obsidian/elements/Divider.tsx
@@ -1,12 +1,30 @@
 "use client";
 
-export default function Divider({ text }: { text?: string }) {
+export default function Divider({
+  text,
+  marginTop = 0,
+  marginBottom = 0,
+}: {
+  text?: string;
+  marginTop?: number;
+  marginBottom?: number;
+}) {
+  const marginStyle = {
+    marginTop: `${marginTop}px`,
+    marginBottom: `${marginBottom}px`,
+  };
+
   if (!text || text.length === 0) {
-    return <div className="w-full h-[4px] border-[rgb(40,40,40)] border" />;
+    return (
+      <div
+        style={marginStyle}
+        className="w-full min-h-1 h-[4px] border-[rgb(40,40,40)] border"
+      />
+    );
   }
 
   return (
-    <div className="w-full flex items-center gap-1">
+    <div style={marginStyle} className="w-full flex items-center gap-1">
       <div className="flex-grow h-[4px] border-[rgb(40,40,40)] border" />
       <span className="text-[12px] text-white/50 whitespace-nowrap leading-none">
         {text}
