e6bd70c0302813742e4ff87ef557d188b4d9a74f
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2026-06-04 21:08:48 +0200

    feat: new reseller

--- src/components/PaymentItem.tsx
+++ src/components/PaymentItem.tsx
@@ -7,7 +7,7 @@ export function PaymentItem({ icon, name, children }: { icon: string; name: stri
                 // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
                 <img src={`/images/icons/${icon}`} className={"absolute align-middle object-contain w-6 h-6"} style={{ margin: "0" }} />
             }
-            <span className={"m-8"}>{name}{children}</span>
+            <span className={"m-8 !mr-0"}>{name}{children}</span>
         </div>
     )
 }
